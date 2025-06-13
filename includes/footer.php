9b6b44d88be9217c723f1eafb800033c43efec51
Bruno Mendez <mendezbruno140@gmail.com>
2025-06-13 17:14:24 -0300

    Mejorado el header, creacion de un archivo css solo para el header, arreglo de nav en responsive

--- includes/footer.php
+++ includes/footer.php
@@ -4,9 +4,5 @@
     <p class="mb-0">📧 Contacto: <a href="mailto:noxbyte.of@gmail.com">noxbyte.of@gmail.com</a></p>
   </div>
 </footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-SvWIf0XJL7wDDqd9d15qg6KGkEVfTl6lR6c50v4+oE2+p5TwqOETJ+5uVr6UQkkY" 
-        crossorigin="anonymous"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,4 @@
-<?php
-// includes/header.php
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,9 +7,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 <!-- etiqueta que puse para que no se visualice una version anterior de la pagina guardada en cache al trabajar e intentar verla en el navegador, se puede sacar despues -->
   <meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
 <!-- !! -->
+ <link rel="stylesheet" href="assets/css/header.css">
 <link rel="stylesheet" href="assets/css/style.css">
   <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
@@ -19,30 +20,36 @@
 </head>
 
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-custom">
-    <div class="container-fluid">
-      <img src="assets/img/logo-draftosaurus.png" alt="Draftosaurus Logo" class="logo-navbar" style="width: 150px; height: 200;margin-left: 30px;">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse w-100" id="navbarNav">
-        <ul class="navbar-nav ms-auto text-center w-100 d-lg-flex justify-content-lg-end">
-          <li class="nav-item ms-3">
-            <a class="nav-link" href="#">Inicio</a>
-          </li>
-          <li class="nav-item ms-3">
-            <a class="nav-link" href="#">Seguimiento</a>
-          </li>
-          <li class="nav-item ms-3">
-            <a class="nav-link" href="#">Registrarse</a>
-          </li>
-          <li class="nav-item ms-3">
-            <a class="nav-link btn-login mt-2 mt-lg-0" href="#">Iniciar sesión</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+ <header class="header">
+		<nav class="navbar  bg-custom navbar-expand-lg position-relative">
+			<div class="container-fluid">
+				<img src="../assets/img/logo-draftosaurus.png" alt="Logo Draftosaurus" class="logo">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+					data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#">Inicio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Seguimiento</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Juego</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Registrarse</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link btn-login" href="#">Iniciar Sesión</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
 </body>
 
