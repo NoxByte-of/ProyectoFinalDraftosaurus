ed441f8eff97be60af77905d4c9f148de2be427b
Bruno Mendez <mendezbruno140@gmail.com>
2025-06-16 13:44:04 -0300

    Cancelamos el uso del Includes por mala compatibilidad

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-custom text-center text-lg-start mt-5">
-  <div class="container p-4">
-    <p class="mb-1">© <?= date("Y") ?> NoxByte. Todos los derechos reservados.</p>
-    <p class="mb-0">📧 Contacto: <a href="mailto:noxbyte.of@gmail.com">noxbyte.of@gmail.com</a></p>
-  </div>
-</footer>
-</body>
-</html>
